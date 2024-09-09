1548fb676d2e1b5887c98736ca683fae1b24ffc9
Catalin Buta <catalinbuta12@yahoo.com>
2024-09-10 01:23:17 +0300

    Add dto classes

---
+++ EventsApp.Database/Dtos/Common/EventDto.cs
@@ -0,0 +1,19 @@
+﻿using EventsApp.Database.Entities;
+using EventsApp.Database.Enums;
+
+namespace EventsApp.Database.Dtos.Common
+{
+    public class EventDto : BaseEntity
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public double Price { get; set; }
+        public EventType EventType { get; set; }
+        public int OwnerId { get; set; }
+        public List<int> ParticipantsIds { get; set; } = new List<int>();
+    }
+}
---
+++ EventsApp.Database/Dtos/Common/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using EventsApp.Database.Entities;
+using EventsApp.Database.Enums;
+
+namespace EventsApp.Database.Dtos.Common
+{
+    public class UserDto : BaseEntity
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public Role Role { get; set; }
+    }
+}
